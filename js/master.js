a9501d9e45120b893131ed369a4e6b07cfcfec99
Danielle Butters <daniellebutters@DaniellsMacBook.fc.ca>
2018-11-16 11:02:56 -0500

    converted to vue.js

--- js/master.js
+++ js/master.js
@@ -1,37 +1,52 @@
 (() => {
     //rewokr this with a vue instance
+        const vm = new Vue({
+            el : "#app",
+            data : {
+                modelname : "",
+                modelpricing : "",
+                modeldetails : "",
 
-    const cars = document.querySelectorAll('.data-ref');
+            },
 
-    function getData() {
-        let targetURL = `./includes/connect.php?modelNo=${this.id}`;
+            mounted : function(){
+                console.log('view is ready to go on the page');
+                //trigger an ajax call with a mocked click event
+                document.querySelector('#F55').click();
 
-        fetch(targetURL) //go get the data and bring it back! good doggy
-        .then(res => res.json()) // turn the result into a plain JS object
-        .then(data => {
-            console.log(data);
-            //run a function to parse our data
-            showCarData(data[0]); //run a function ot put the data on the page
-        })
-        //let see what we got
-        .catch(function(error) {
-            console.log(error); // if anthing broke, log it to the console
+            },
+
+            beforeUpdate : function(){
+                console.log('things are going to change...');
+            },
+
+            update : function() {
+                console.log('things are different now');
+            },
+
+            methods : {
+                fetchData(e) {
+                    // debugger;
+                    let targetURL = e.currentTarget.id; // gets the id of the element via the event object
+                    
+                    fetch(`./includes/connect.php?modelNo=${targetURL}`) //go get the data and bring it back! good doggy
+                    .then(res => res.json()) // turn the result into a plain JS object
+                    .then(data => {
+                        console.log(data);
+                        const {modelName, pricing, modelDetails} = data[0];
+
+                        this.modelname = modelName;
+                        this.modelpricing = pricing;
+                        this.modeldetails = modelDetails;
+                        //run a function to parse our data
+                        // showCarData(data[0]); //run a function ot put the data on the page
+                    })
+                    //let see what we got
+                    .catch(function(error) {
+                        console.log(error); // if anthing broke, log it to the console
+                    });
+                }
+            }
         });
-    }
-
-    function showCarData(data) {
-        //parse the DB info and put it where it needs to go 
-        const {modelName, pricing, modelDetails} = data; // destructing assignment => MDN JS destructing
-
-        //grab the elements we need, and populate them with data
-        document.querySelector('.modelName').textContent = modelName;
-        document.querySelector('.priceInfo').textContent = `$ ${pricing}.00`;
-        document.querySelector('.modelDetails').textContent = modelDetails;
-    }
-
-    cars.forEach(car => car.addEventListener("click", getData));
-
-    // getData(); //trigger the getData function
-
 
 })();
